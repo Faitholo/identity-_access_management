e25f03a57d4bbd0af6ac1f9e36f60edabd2b668f
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-07-06 20:55:28 +0100

    practice worked

--- Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -4,6 +4,10 @@ from sqlalchemy import exc
 import json
 from flask_cors import CORS
 from functools import wraps
+from jose import jwt
+from urllib.request import urlopen
+from dotenv import load_dotenv
+load_dotenv()
 
 from .database.models import db_drop_and_create_all, setup_db, Drink
 from .auth.auth import AuthError, requires_auth
@@ -12,6 +16,9 @@ app = Flask(__name__)
 setup_db(app)
 CORS(app)
 
+AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
+ALGORITHMS = os.environ.get('ALGORITHMS')
+API_AUDIENCE = os.environ.get('API_AUDIENCE')
 
 #uncomment the following line to initialize the datbase
 #THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
@@ -26,40 +33,117 @@ testing out header requests using postman
 this is just practice
 """
 
-def headers():
-    # @TODO unpack the request header
-    auth = request.headers['Authorization']
-    split_auth = auth.split(" ")
-    
-    if "Authorization" not in request.headers:
-        abort(401)
-    
-    elif len(split_auth) != 2:
-        abort(401)
-    
-    print(split_auth[1])
-    return 'not implemented'
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
 
-def get_header(f):
+
+def get_token_auth_header():
+    """Obtains the Access Token from the Authorization Header
+    """
+    auth = request.headers.get('Authorization', None)
+    if not auth:
+        raise AuthError({
+            'code': 'authorization_header_missing',
+            'description': 'Authorization header is expected.'
+        }, 401)
+
+    parts = auth.split()
+    if parts[0].lower() != 'bearer':
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must start with "Bearer".'
+        }, 401)
+
+    elif len(parts) == 1:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Token not found.'
+        }, 401)
+
+    elif len(parts) > 2:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must be bearer token.'
+        }, 401)
+
+    token = parts[1]
+    return token
+
+
+def verify_decode_jwt(token):
+    jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+    jwks = json.loads(jsonurl.read())
+    unverified_header = jwt.get_unverified_header(token)
+    rsa_key = {}
+    if 'kid' not in unverified_header:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization malformed.'
+        }, 401)
+
+    for key in jwks['keys']:
+        if key['kid'] == unverified_header['kid']:
+            rsa_key = {
+                'kty': key['kty'],
+                'kid': key['kid'],
+                'use': key['use'],
+                'n': key['n'],
+                'e': key['e']
+            }
+    if rsa_key:
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer='https://' + AUTH0_DOMAIN + '/'
+            )
+
+            return payload
+
+        except jwt.ExpiredSignatureError:
+            raise AuthError({
+                'code': 'token_expired',
+                'description': 'Token expired.'
+            }, 401)
+
+        except jwt.JWTClaimsError:
+            raise AuthError({
+                'code': 'invalid_claims',
+                'description': 'Incorrect claims. Please, check the audience and issuer.'
+            }, 401)
+        except Exception:
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to parse authentication token.'
+            }, 400)
+    raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to find the appropriate key.'
+            }, 400)
+
+
+def requires_auth(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
-        jwt = headers()
-        
-        return f(jwt, *args, **kwargs)
+        token = get_token_auth_header()
+        try:
+            payload = verify_decode_jwt(token)
+        except:
+            abort(401)
+        return f(payload, *args, **kwargs)
+
     return wrapper
 
-@app.route('/headers')
-@get_header
-def test_header(jwt):
-    print(jwt)
-    return "Not implemented"
-
-
-
-
 @app.route('/home')
-def test():
-    return "Test worked!"
+@requires_auth
+def headers(payload):
+    print(payload)
+    return 'Access Granted'
+
 
 '''
 @TODO implement endpoint
